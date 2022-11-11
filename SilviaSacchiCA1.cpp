2226cac4a7e8b2e134b12b7bc33ccda1e1306262
silviasacchi <silvi.sak93@gmail.com>
2022-11-11 12:27:41 -0800

    ForthCommit

--- SilviaSacchiCA1.cpp
+++ SilviaSacchiCA1.cpp
@@ -8,24 +8,36 @@
 struct bullets
 {
     Rectangle rect; //rectangle.
-    Vector2 speed; //vectore that interpolate x and y.
-    bool active;  //bollean to establish if the bullest are active or not.
+    Vector2 speed; //vectore that interpolates x and y.
+    bool active;  //boollean to establish if the bullest are active or not.
     Color color; //bullet colour.
+};
+
+//creating structure for enemies
+struct enemies
+{
+    Rectangle rect; //rectangle.
+    Vector2 speed; //vectore that interpolates x and y. 
+    bool active; //boolean that indicates if the enemy is alive or not.
+    Color color; //enemies' colour.
 };
 
 //main funcion
 int main(){
     const int windowWidth {500}; //the window width is a constant integer because it does never change.
     const int windowHeight {500}; //the window height is a constant integer because it does never change.
-    int num_bullets {50};  //estaglishing array as integers for the number of bullets.
+    int num_bullets {50}; //estaglishing array as integers for the number of bullets.
+    int shootRate {}; //setting up integer variable for the shooting rate. We put it at 0 for the moment.
+    int num_enemies {30}; //esablishing array as integers for the number of enemies.
+    int activeEnemies {}; //setting up integer variable for the number of enemies active at a certain point in time.
 
     InitWindow (windowWidth, windowHeight, "Space Invaders"); //Creating window using the e parameters created above.
     
     Texture2D spaceship = LoadTexture ("resources/spaceship.png"); //leading the png texture for the spaceship.
 
     //creating rectangle for the spaceship
-    Rectangle spaceshipRec; 
-    Vector2 spaceshipSpeed;
+    Rectangle spaceshipRec; //ceating the spaceship rectangle.
+    Vector2 spaceshipSpeed; //creating the spaceship speed vector.
     spaceshipRec.x = windowWidth/2; //setting up the spaceship rectangle position on the x axis.
     spaceshipRec.y = windowHeight; //setting up the spaceship rectangle position on the y axis.
     spaceshipRec.width = spaceship.width; //setting up the spaceship rectangle width.
@@ -33,7 +45,7 @@ int main(){
     spaceshipSpeed.x = windowWidth/2 - spaceshipRec.width/2; //setting up the speed movement on the x axis.
     spaceshipSpeed.y = windowHeight - spaceshipRec.height; //setting up the speed movemtn on the y axis.
 
-    //creating bullest rectangle using the structure created in the beginning
+    //creating bullet rectangle using the bullets structure
     bullets bullet [num_bullets];
     //using for loop so that I don't need to initialise all 50 bullets one by one.
     for (int i = 0; i < num_bullets; i++) //i represents a specific bullet i from i to 50, assuming an increased value for each loop iteration.
@@ -49,9 +61,22 @@ int main(){
         bullet[i].active = false;
     }
 
-    int shootRate {}; //setting up integer variable for the shooting rate. We put it at 0 for the moment.
-
-
+    //creating enemy rectangle using the enemies structure
+    enemies enemy [num_enemies];
+    //setting up the number of acie enemies.
+    activeEnemies = 6;
+    //using for loop so that I don't have to initialised all 30 enemies one by one.
+    for (int i = 0; i < num_enemies; i++)
+    {
+        enemy[i].rect.width = 20;
+        enemy[i].rect.height = 20;
+        enemy[i].rect.x = GetRandomValue (0, windowWidth); //enemy position on the x axis to be a random number between 0 and the winwow width.
+        enemy[i].rect.y = GetRandomValue (-windowHeight, -20); //enemy position on the y axis to be a random number between the window height and 20.
+        enemy[i].speed.x = 2;
+        enemy[i].speed.y = 2;
+        enemy[i].active = true;
+        enemy[i].color = BLUE;
+    }
 
     SetTargetFPS (60); //setting up the number of frame per second.
     
@@ -65,8 +90,30 @@ int main(){
         //for loop to draw the bullet rectangle for each and every bullet.
         for (int i = 0; i < num_bullets; i++)
         {
-            if (bullet[i].active) //if a certaing bullet i between 1 and 50 is active.
-            DrawRectangleRec (bullet[i].rect, bullet[i].color);
+            if (bullet[i].active) //if a certaing bullet is between 1 and 50 is active.
+            DrawRectangleRec (bullet[i].rect, bullet[i].color); //drawing the bullet rectangle.
+        }
+
+        //for loop to make the enemies move
+        for (int i = 0; i < activeEnemies; i++)
+        {
+            if (enemy[i].active) //if the enemy is active.
+            {
+                enemy[i].rect.y += enemy[i].speed.y; //new position of enemy on the y axis is its old position plus its speed on the y axis.
+                //reset is the enemy crosses the boundary
+                if (enemy[i].rect.y > windowHeight) //i the enemy goes beyond the bottom od the window.
+                {
+                    enemy[i].rect.x = GetRandomValue (0, windowWidth); //new enemy position on the x axis.
+                    enemy[i].rect.y = GetRandomValue (-windowHeight, -20); //new enemy position on the y axis.
+                }
+            }
+        }
+
+        //for loop to draw the enemy bullet for each and every enemy.
+        for (int i = 0; i < activeEnemies; i++)
+        {
+            if (enemy[i].active) //if the enemy is active.
+            DrawRectangleRec (enemy[i].rect, enemy[i].color); //drawing the enemy rectangle.
         }
 
         //shooting bullets
@@ -76,11 +123,11 @@ int main(){
             //creating for loop to make bullets move
             for (int i = 0; i < num_bullets; i++)
             {
-                if (!bullet[i].active && shootRate % 40 ==0) //if a certain bullet i is active and the shooting rate reminder of the division by 40 is 0;
+                if (!bullet[i].active && shootRate % 40 ==0) //if a certain bullet i is active and the shooting rate remainder of the division by 40 is 0;
                 {
-                    bullet[i].rect.x = spaceshipSpeed.x;
-                    bullet[i].rect.y = spaceshipSpeed.y + spaceshipRec.height/4;
-                    bullet[i].active = true;
+                    bullet[i].rect.x = spaceshipSpeed.x; //the position of the bullet on the x axis is now equal to it's speed on the x axis.
+                    bullet[i].rect.y = spaceshipSpeed.y + spaceshipRec.height/4;  //the position on the bullet on the y axix is now equal tp its speed on the y axis plus its size divided by four.
+                    bullet[i].active = true; //the bullet is active.
                     break; //exiting the loop despite we don't know how many iterations are needed in the loop.
                 }
             }
@@ -88,13 +135,13 @@ int main(){
 
         for (int i = 0; i < num_bullets; i++)
         {
-            if (bullet[i].active)
+            if (bullet[i].active) //if the bullet is active.
             {
-                bullet[i].rect.y += bullet[i].speed.y;
-                if (bullet[i].rect.y <= 0)
+                bullet[i].rect.y += bullet[i].speed.y; //the bullet position on the y axis is now equal to its previous position plus its speed.
+                if (bullet[i].rect.y <= 0) //if the bullet position on the y axis is below or equal 0.
                 {
-                    bullet[i].active = false;
-                    shootRate = 0;
+                    bullet[i].active = false; //the bullet is not active.
+                    shootRate = 0; //the shooting rate is 0.
                 } 
             }
         }
@@ -108,8 +155,8 @@ if (IsKeyDown (KEY_RIGHT)) //if the right key is down.
 {
     spaceshipSpeed.x += 3; //the spaceship speed should inclease by 3.
 }
-if (spaceshipRec.x < 0 || spaceshipRec.x > windowWidth - spaceshipRec.width) {
-    spaceshipSpeed.x = 0;
+if (spaceshipRec.x < 0 || spaceshipRec.x > windowWidth - spaceshipRec.width) { //if the spaceship position on the x axis is below 0 or greater than the window width minus the spaceship width.
+    spaceshipSpeed.x = 0; //the spaceship speed on the x axis is 0.
 }
 
         ClearBackground (WHITE); //clear the backgroung.
